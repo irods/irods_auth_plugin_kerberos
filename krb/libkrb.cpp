3605720b8d8f56630201f3731dc853b9824fceb9
Zoey Greer <tempoz@renci.org>
2015-01-21 15:35:53 -0500

    [#4] moved krbAuthRequest.hpp; fixed build

--- krb/libkrb.cpp
+++ krb/libkrb.cpp
@@ -948,7 +948,7 @@ extern "C" {
                                acGetUserByDN(*arg,*OUT)||msiExecCmd(t,"*arg",null,null,null,*OUT)|nop
                             */
                             ruleExecInfo_t rei;
-                            char *args[2];
+                            const char *args[2];
                             msParamArray_t *myMsParamArray;
                             msParamArray_t myInOutParamArray;
 
