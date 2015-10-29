81457993cbee60dbdfb190a06be4dcd7e8cf06db
Antoine de Torcy <adetorcy@email.unc.edu>
2015-10-29 12:13:37 -0400

    [#7] Update tests for iRODS 4.1.6

--- packaging/test_irods_auth_plugin_krb.py
+++ packaging/test_irods_auth_plugin_krb.py
@@ -1,62 +1,153 @@
 import os
-import commands
-import time
-
 import sys
-if sys.version_info >= (2,7):
+if sys.version_info >= (2, 7):
     import unittest
 else:
     import unittest2 as unittest
-
+import shutil
+import json
 import lib
-from resource_suite import ResourceBase
 
 
-class Test_Kerberos(ResourceBase, unittest.TestCase):
+# JSON config file must have:
+#    'client_user_principal'
+#    'client_user_ticket_cache'
+#    get both from klist
+CFG_FILE_PATH = '/tmp/krb5_test_cfg.json'
+
+class Test_Authentication(unittest.TestCase):
+
+    '''Tests kerberos authentication for a regular user.
+    '''
+
     def setUp(self):
-        super(Test_Kerberos, self).setUp()
-        keytabFileSource = "~/secrets/kerberos/irods.keytab"
-        keytabFileDest = "/var/lib/irods/irods.keytab"
-        hostname = lib.get_hostname()
-        serverConfig = "/etc/irods/server.config"
-        irodsUser = "rods"
-        irodsUserDN = "irods@IRODS.RENCI.ORG"
+        super(Test_Authentication, self).setUp()
 
-        # Copy the keytab file to the irods install area
-        os.system("cp %s %s" % (keytabFileSource, keytabFileDest))
-        os.system("chmod 600 %s" % keytabFileDest)
+        # load configuration from file
+        with open(CFG_FILE_PATH) as cfg_file:
+            self.config = json.load(cfg_file)
 
-        # Edit the server config to set the name of the irods service
-        print "iRODS Server Hostname: ", hostname
-        os.system("echo \"KerberosServicePrincipal irods-server/%s@IRODS.RENCI.ORG\" >> %s" % (hostname, serverConfig))
-        os.system("echo \"KerberosKeytab %s\" >> %s" % (keytabFileDest, serverConfig))
+        # local test dir
+        self.testing_tmp_dir = '/tmp/irods_Test_Authentication'
+        shutil.rmtree(self.testing_tmp_dir, ignore_errors=True)
+        os.mkdir(self.testing_tmp_dir)
 
-        # Add the user authentication
-        os.system("iadmin aua %s %s" % (irodsUser, irodsUserDN))
+        # admin sesh
+        self.admin = lib.make_session_for_existing_admin()
 
-        self.prev_auth_scheme = os.environ.get('IRODS_AUTHENTICATION_SCHEME', None)
-        os.environ['IRODS_AUTHENTICATION_SCHEME'] = 'krb'
+        # make new user with no password
+        self.krb_username = 'krb_user'
+        self.krb_user = lib.mkuser_and_return_session(
+            'rodsuser', self.krb_username, None, lib.get_hostname())
+
+        # set auth scheme for new user (client side)
+        self.krb_user.environment_file_contents[
+            'irods_authentication_scheme'] = 'krb'
+
+        # set auth string for new user (server side)
+        self.admin.run_icommand(['iadmin', 'aua', self.krb_username, self.config['client_user_principal']])
 
     def tearDown(self):
-        if self.prev_auth_scheme:
-            os.environ['IRODS_AUTHENTICATION_SCHEME'] = self.prev_auth_scheme
-        super(Test_Kerberos, self).tearDown()
+        # exit user sesh
+        self.krb_user.__exit__()
 
-    # Try to authenticate before getting a TGT. Make sure this fails.
-    def test_authentication_krb_without_tgt(self):
-        # Destroy any existing TGT's
-        os.system("kdestroy")
+        # remove krb user
+        self.admin.run_icommand(['iadmin', 'rmuser', self.krb_username])
 
-        # Try an ils and make sure it fails
-        self.admin.assert_icommand("ils", 'STDERR_SINGLELINE', "KRB_ERROR_ACQUIRING_CREDS")
+        # exit admin sesh
+        self.admin.__exit__()
 
-    # Try to authenticate after getting a TGT. This should pass
-    def test_authentication_krb_with_tgt(self):
-        krbPassword = "thisisnotasecret"
+        # remove local test dir
+        shutil.rmtree(self.testing_tmp_dir)
 
-        # Make sure we have a valid TGT
-        os.system("echo Password: \"%s\"" % krbPassword)
-        os.system("echo %s | kinit" % krbPassword)
+        super(Test_Authentication, self).tearDown()
 
-        # Try an ils
-        self.admin.assert_icommand("ils", 'STDOUT_SINGLELINE', "home")
+    def test_ils(self):
+        # set client env
+        env = os.environ.copy()
+        env['KRB5CCNAME'] = self.config['client_user_ticket_cache']
+
+        self.krb_user.assert_icommand(
+            'ils', 'STDOUT_SINGLELINE', self.krb_user.home_collection, env=env)
+
+    def test_irsync_r_nested_dir_to_coll(self):
+        # test settings
+        depth = 10
+        files_per_level = 100
+        file_size = 100
+
+        # make local nested dirs
+        base_name = "test_irsync_r_nested_dir_to_coll"
+        local_dir = os.path.join(self.testing_tmp_dir, base_name)
+        local_dirs = lib.make_deep_local_tmp_dir(
+            local_dir, depth, files_per_level, file_size)
+
+        # set client env
+        env = os.environ.copy()
+        env['KRB5CCNAME'] = self.config['client_user_ticket_cache']
+
+        # sync dir to coll
+        self.krb_user.assert_icommand(
+            "irsync -r {local_dir} i:{base_name}".format(**locals()), "EMPTY", env=env)
+
+        # compare files at each level
+        for directory, files in local_dirs.iteritems():
+            partial_path = directory.replace(self.testing_tmp_dir + '/', '', 1)
+
+            # run ils on subcollection
+            self.krb_user.assert_icommand(
+                ['ils', partial_path], 'STDOUT_SINGLELINE', env=env)
+            ils_out = lib.ils_output_to_entries(
+                self.krb_user.run_icommand(['ils', partial_path], env=env)[1])
+
+            # compare local files with irods objects
+            local_files = set(files)
+            rods_files = set(lib.files_in_ils_output(ils_out))
+            self.assertTrue(local_files == rods_files,
+                            msg="Files missing:\n" + str(local_files - rods_files) + "\n\n" +
+                            "Extra files:\n" + str(rods_files - local_files))
+
+        # cleanup
+        self.krb_user.assert_icommand(
+            "irm -rf {base_name}".format(**locals()), "EMPTY", env=env)
+
+    def test_irsync_r_nested_dir_to_coll_large_files(self):
+        # test settings
+        depth = 4
+        files_per_level = 4
+        file_size = 1024 * 1024 * 40
+
+        # make local nested dirs
+        base_name = "test_irsync_r_nested_dir_to_coll"
+        local_dir = os.path.join(self.testing_tmp_dir, base_name)
+        local_dirs = lib.make_deep_local_tmp_dir(
+            local_dir, depth, files_per_level, file_size)
+
+        # set client env
+        env = os.environ.copy()
+        env['KRB5CCNAME'] = self.config['client_user_ticket_cache']
+
+        # sync dir to coll
+        self.krb_user.assert_icommand(
+            "irsync -r {local_dir} i:{base_name}".format(**locals()), "EMPTY", env=env)
+
+        # compare files at each level
+        for directory, files in local_dirs.iteritems():
+            partial_path = directory.replace(self.testing_tmp_dir + '/', '', 1)
+
+            # run ils on subcollection
+            self.krb_user.assert_icommand(
+                ['ils', partial_path], 'STDOUT_SINGLELINE', env=env)
+            ils_out = lib.ils_output_to_entries(
+                self.krb_user.run_icommand(['ils', partial_path], env=env)[1])
+
+            # compare local files with irods objects
+            local_files = set(files)
+            rods_files = set(lib.files_in_ils_output(ils_out))
+            self.assertTrue(local_files == rods_files,
+                            msg="Files missing:\n" + str(local_files - rods_files) + "\n\n" +
+                            "Extra files:\n" + str(rods_files - local_files))
+
+        # cleanup
+        self.krb_user.assert_icommand(
+            "irm -rf {base_name}".format(**locals()), "EMPTY", env=env)
